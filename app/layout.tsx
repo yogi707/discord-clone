f958084a68f6207e7fdd79f9f7954b9ad8af4b29
yogi707 <40931290+yogi707@users.noreply.github.com>
2023-09-17 20:00:58 +0530

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,9 +43,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider
